4f5bca39b56f4eb0daf33f560e450067e5100db0
Mohd Sameer <mohdsameer1310@gmail.com>
2023-11-02 18:30:02 +0530

    Fixed some issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   # Association
-  has_many :order_products
+  has_many :order_products, dependent: :destroy
   has_many :orders, through: :order_products
-  has_many :variants
-  has_many :product_producer_pricings
+  has_many :variants, dependent: :destroy
+  has_many :product_producer_pricings, dependent: :destroy
 
   # Nested Attributes
   accepts_nested_attributes_for :variants, reject_if: :all_blank, allow_destroy: true
