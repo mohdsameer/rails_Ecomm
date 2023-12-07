b48976c9618342d7742ff434f8e6c47dfa358c23
Mohd Sameer <mohdsameer1310@gmail.com>
2023-12-07 11:35:42 +0530

    Uncommented that commented migrations

--- db/migrate/20231117101654_add_fieldname_to_variant.rb
+++ db/migrate/20231117101654_add_fieldname_to_variant.rb
@@ -1,5 +1,5 @@
 class AddFieldnameToVariant < ActiveRecord::Migration[7.0]
   def change
-  	# add_column :variants, :archive, :boolean, default: false
+  	add_column :variants, :archive, :boolean, default: false
   end
 end
